cab1c4f8234f82ca53add5ca786beeb0609175d2
Ренат Саматов <awakentwice@gmail.com>
2016-08-21 21:38:15 +0300

    Added employees list

---
+++ src/constants/index.js
@@ -0,0 +1,50 @@
+export const EMPLOYEES_LIST = [
+  {
+    id: 1,
+    name: 'Игорь',
+    email: 'igor@email.com',
+    gender: 1,
+  },
+  {
+    id: 2,
+    name: 'Ольга',
+    email: 'olga@email.com',
+    gender: 0,
+  },
+  {
+    id: 3,
+    name: 'Константин',
+    email: 'konstantin@email.com',
+    gender: 1,
+  },
+  {
+    id: 4,
+    name: 'Евгений',
+    email: 'eugene@email.com',
+    gender: 1,
+  },
+  {
+    id: 5,
+    name: 'Светлана',
+    email: 'svetlana@email.com',
+    gender: 0,
+  },
+  {
+    id: 6,
+    name: 'Павел',
+    email: 'paul@email.com',
+    gender: 1,
+  },
+  {
+    id: 7,
+    name: 'Александр',
+    email: 'alex@email.com',
+    gender: 1,
+  },
+  {
+    id: 8,
+    name: 'Юлия',
+    email: 'julia@email.com',
+    gender: 0,
+  },
+]
